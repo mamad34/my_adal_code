1388b5384556d905d49c1833bff85332ba26fa02
Mohammad Thr <36333486+mamad34@users.noreply.github.com>
2023-03-11 15:26:31 +0330

    added new sql

--- neglected/SQL/Neglected.ts
+++ neglected/SQL/Neglected.ts
@@ -6,27 +6,27 @@ export const amazonNeglectedSql = ({
   after,
   details,
 }: MakeFirstAfterNullable<Dius.QueryAmazonNeglectedProductsArgs>) => {
-  let sql = `SELECT  p.id, p.sku, p.big_image, p.small_image,
-                p.asin, nd.neglected_details, apd.neglected_status
-                FROM products as p 
-                LEFT JOIN LATERAL (
-                  SELECT JSON_AGG(
-                      JSON_BUILD_OBJECT(
-                        'id', andd.id,
-                        'date', TO_CHAR(andd.operation_date, 'mm/dd/yyyy'),
-                        'operationMode', andd.operation_mode,
-                        'note', andd.note
-                      ) 
-                  ) AS neglected_details
-                  FROM amazon_neglected_details AS andd
-                  WHERE andd.sku_id = p.id
-                  GROUP BY sku_id
-                ) AS nd
-                ON true
-                LEFT JOIN amazon_product_details AS apd
-                ON apd.id = p.id 
-                WHERE p.store_id = $1
-                `;
+  let sql = `SELECT p.id, p.sku, p.big_image, p.small_image,
+              p.asin, nd.neglected_details, apd.neglected_status
+              FROM products as p
+              LEFT JOIN LATERAL (
+                SELECT JSON_AGG(
+                  JSON_BUILD_OBJECT(
+                    'id', andd.id,
+                    'date', TO_CHAR(andd.operation_date, 'mm/dd/yyyy'),
+                    'operationMode', andd.operation_mode,
+                    'note', andd.note
+                  )
+                ) AS neglected_details
+                FROM amazon_neglected_details AS andd
+                WHERE andd.sku_id = p.id
+                GROUP BY sku_id
+              ) AS nd
+              ON true
+              LEFT JOIN amazon_product_details AS apd
+              ON apd.id = p.id 
+              WHERE p.store_id = $1
+              `;
 
   if (details.neglectedFilter) {
     sql += `AND apd.neglected_status = '${details.neglectedFilter}'
@@ -34,12 +34,22 @@ export const amazonNeglectedSql = ({
   }
 
   if (details.searchTerm) {
-    sql += `AND (p.sku ILIKE '%${details.searchTerm}%')
-            AND (apd.neglected_status  '%${details.searchTerm}%')
+    sql += `AND (p.sku ILIKE '%${details.searchTerm}%'
+            OR apd.neglected_status ILIKE '%${details.searchTerm}%')
           `;
   }
 
-  sql += `ORDER BY ${details.sort.columnName} ${details.sort.sortBy} NULLS LAST, p.id
+  sql += `ORDER BY ${
+    details.sort.columnName !==
+    ('apd.neglected_status' as Dius.AmazonNeglectedProductsSortableColumns)
+      ? `CASE WHEN apd.neglected_status = 'OPTIMIZE' THEN 1
+          WHEN apd.neglected_status = 'DELETE_SKU' THEN 2
+          WHEN apd.neglected_status = 'KEEP_SELLING' THEN 3
+          ELSE 4
+        END,`
+      : ''
+  }
+        ${details.sort.columnName} ${details.sort.sortBy} NULLS LAST, p.id
          `;
 
   if (first !== null && after !== null) {
@@ -54,4 +64,4 @@ export const getMaxRowsOfSkuSql = `SELECT COUNT(sku_id) AS total_columns
                                     WHERE store_id = $1
                                     GROUP BY sku_id
                                     ORDER BY COUNT(sku_id) DESC
-                                    LIMIT 1`;
+                                    LIMIT 1;`;
